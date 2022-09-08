81b4a5f3bcc79a494667520eaed99e488f2548dd
Eric Lowe <ericlowe25@gmail.ocm>
2022-09-07 22:09:08 -0400

    initial commit

--- 1.py
+++ 1.py
@@ -1,1 +1,9 @@
 # Write your solution for algorithm 1 below
+list = [5,9,1,8]
+
+def sort_1(lst):
+    return lst.sort()
+
+print(list)
+sort_1(list)
+print(list)
--- 3.py
+++ 3.py
@@ -1,1 +1,7 @@
 # Write your solution for algorithm 3 below
+def sort_3(lst):
+    return sorted(lst, reverse=True)
+
+sample_list3 = [55,23,8,79]
+
+print(sort_3(sample_list3))
